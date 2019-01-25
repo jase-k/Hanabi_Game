a0addb97de6de06d0f82562f95e1047276eaf133
Jase Kraft <jasekraft12@gmail.com>
2019-01-24 19:59:01 -0600

    Added css for The User's Hand

--- src/components/game/user/user.js
+++ src/components/game/user/user.js
@@ -6,10 +6,88 @@ export default class User extends React.Component {
   }
   render(){
     return(
-      <div className="game-container">
-      The User Goes Here:
+    <div id="cardHolder" class="card-holder flex">
+     <div  class="card">
+       <h3>
+         Card 1
+       </h3>
+       <div class="notes">
+         <h4>
+           Hints Given
+         </h4>
 
-      </div>
+         </div>
+       <div class="personal notes">
+         <h4>
+           Personal Notes
+         </h4>
+       </div>
+     </div>
+      <div  class="card">
+       <h3>
+         Card 2
+       </h3>
+       <div class="notes">
+         <h4>
+           Hints Given
+         </h4>
+
+         </div>
+       <div class="personal notes">
+         <h4>
+           Personal Notes
+         </h4>
+       </div>
+     </div>
+      <div  class="card">
+       <h3>
+         Card 3
+       </h3>
+       <div class="notes">
+         <h4>
+           Hints Given
+         </h4>
+
+         </div>
+       <div class="personal notes">
+         <h4>
+           Personal Notes
+         </h4>
+       </div>
+     </div>
+      <div  class="card">
+       <h3>
+         Card 4
+       </h3>
+       <div class="notes">
+         <h4>
+           Hints Given
+         </h4>
+
+         </div>
+       <div class="personal notes">
+         <h4>
+           Personal Notes
+         </h4>
+       </div>
+     </div>
+      <div  id="optional-card" class="card">
+       <h3>
+         Card 5
+       </h3>
+       <div class="notes">
+         <h4>
+           Hints Given
+         </h4>
+
+         </div>
+       <div class="personal notes">
+         <h4>
+           Personal Notes
+         </h4>
+       </div>
+     </div>
+   </div>
     )
   }
 }
