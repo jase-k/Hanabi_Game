4cd9dd998c9aa0b5295bb76d310c6d907471c6d4
Jase Kraft <jasekraft12@gmail.com>
2019-01-26 08:26:42 -0600

    Updates

---
+++ precache-manifest.cf539600657687e106dcd07b87f9846e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44fe9ec360a523f687b3",
+    "url": "/Hanabi_Game/static/js/runtime~main.44fe9ec3.js"
+  },
+  {
+    "revision": "832a2413c4509d59a22b",
+    "url": "/Hanabi_Game/static/js/main.832a2413.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/Hanabi_Game/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "832a2413c4509d59a22b",
+    "url": "/Hanabi_Game/static/css/main.6b6f23c2.chunk.css"
+  },
+  {
+    "revision": "bc06974e8926bdf56e366bb9890981a7",
+    "url": "/Hanabi_Game/index.html"
+  }
+];
