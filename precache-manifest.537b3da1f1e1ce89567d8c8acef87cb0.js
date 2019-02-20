368e979a0d8f771660d9f3e2f5fbe7c7d04b1481
Jase Kraft <jasekraft12@gmail.com>
2019-02-20 10:51:30 -0600

    Updates

--- precache-manifest.537b3da1f1e1ce89567d8c8acef87cb0.js
+++ precache-manifest.537b3da1f1e1ce89567d8c8acef87cb0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Hanabi_Game/static/js/runtime~main.44fe9ec3.js"
   },
   {
-    "revision": "d023a7aaad527fbf1bbf",
-    "url": "/Hanabi_Game/static/js/main.d023a7aa.chunk.js"
+    "revision": "020f4968648bd59e96dd",
+    "url": "/Hanabi_Game/static/js/main.020f4968.chunk.js"
   },
   {
     "revision": "07140812a98c93f90dd2",
     "url": "/Hanabi_Game/static/js/1.07140812.chunk.js"
   },
   {
-    "revision": "d023a7aaad527fbf1bbf",
+    "revision": "020f4968648bd59e96dd",
     "url": "/Hanabi_Game/static/css/main.242232c5.chunk.css"
   },
   {
-    "revision": "6c701017dc4c3a94b7c367dc238d8487",
+    "revision": "5a29fdea399a252bf22c9ee284ac404d",
     "url": "/Hanabi_Game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Hanabi_Game/precache-manifest.ad89d155f18b00b7972de18e9ea1bf1e.js"
+  "/Hanabi_Game/precache-manifest.537b3da1f1e1ce89567d8c8acef87cb0.js"
 );
 
 workbox.clientsClaim();
